6c085842d4058fd16dd7ce9625fe0e173d24878a
Takao Kamenoue <totocalcio34@gmail.com>
2019-05-09 11:50:33 +0900

    変数とcssを調整

--- Task1/js/main.js
+++ Task1/js/main.js
@@ -1,15 +1,15 @@
 function disp(){
 	'use strict'
 
-	var num = prompt('数字を入力してください');
+	let num = prompt('数字を入力してください');
 
 	if ( num === null || num === ''){
 		console.log('入力が空かキャンセルされました');
 	} else if (num.match(/^[1-9][0-9]+$/)){
-		var str;
-		var ary = new Array();
+		let str;
+		let ary = new Array();
 		function* generator(from, to){ while(from <= to) yield from++; }
-		var gen = generator(1, num);
+		let gen = generator(1, num);
 		for(num of gen) {
 			if (num%3 === 0 && num%5 === 0) {
 				str = 'FizzBuzz';
--- Task2/js/main.js
+++ Task2/js/main.js
@@ -1,13 +1,13 @@
 function disp(){
 	'use strict'
 
-	var str = prompt('文字列を入力してください');
+	const str = prompt('文字列を入力してください');
 
 	if ( str === null || str === ''){
 		console.log('入力が空かキャンセルされました');
 		return;
 	}
-	var ary = str.split(/\s+/);
+	const ary = str.split(/\s+/);
 
 	function count(ary){
 		let check = {};
