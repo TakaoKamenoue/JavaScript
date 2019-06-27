9a956707ec023fd06cf5c45f78baeb04ed333fed
TakaoKamenoue <49269319+TakaoKamenoue@users.noreply.github.com>
2019-06-27 18:07:09 +0900

    Update main.js
    try-catchだったのを、エラー時breakで処理を終了するように修正

--- Task3/js/main.js
+++ Task3/js/main.js
@@ -6,34 +6,31 @@ function disp(){
 	let flg = 0;	//0:あいこ 1:決着
 	const rps = ['グー','チョキ','パー'];
 
-	try {
-		do {
-			console.log('「じゃんけん・・・」');
-			let hum = prompt('「じゃんけん・・・」\n0.グー 1.チョキ 2.パー')
 
-			if (hum === null) {
-				console.warn('入力がキャンセルされました');
-				break;
-			} else if (!(hum in rps)) {
-				throw new Error('0,1,2で入力して下さい');
-			}
+	do {
+		console.log('「じゃんけん・・・」');
+		let hum = prompt('「じゃんけん・・・」\n0.グー 1.チョキ 2.パー')
 
-			let cpu = Math.floor(Math.random() * rps.length);	
-			console.log('「ぽい！」');
-			console.log('*コンピューター：'+rps[cpu]);
-			console.log('*あなた：'+rps[hum]);
+		if (hum === null) {
+			console.warn('入力がキャンセルされました');
+			break;
+		} else if (!(hum in rps)) {
+			console.error('0,1,2で入力して下さい')
+		}
 
-			if (win.includes(hum - cpu)){
-				console.log('「あなたの勝ち！」');
-				flg = 1;
-			} else if (lose.includes(hum - cpu)){
-				console.log('「あなたの負け・・・」');
-				flg = 1;
-			} else {
-				console.log('アイコでしょ！');
-			}
-		} while (flg === 0);
-	} catch (e) {
-		console.error( e.message );
-	}
+		let cpu = Math.floor(Math.random() * rps.length);	
+		console.log('「ぽい！」');
+		console.log('*コンピューター：'+rps[cpu]);
+		console.log('*あなた：'+rps[hum]);
+
+		if (win.includes(hum - cpu)){
+			console.log('「あなたの勝ち！」');
+			flg = 1;
+		} else if (lose.includes(hum - cpu)){
+			console.log('「あなたの負け・・・」');
+			flg = 1;
+		} else {
+			console.log('アイコでしょ！');
+		}
+	} while (flg === 0);
 };
